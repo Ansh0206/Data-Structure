fbfbb80b7965f20cdcbad5556a97cf677f5ac843
Ansh <148424504+Ansh0206@users.noreply.github.com>
2025-11-03 16:26:37 +0530

    Time: 5 ms (77.95%), Space: 160 MB (6.11%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,43 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int less=0;
-        int greater=0;
-        int equal=0;
-        for(int i : nums){
-            if(i==pivot){
-                equal++;
-            }else if(i>pivot){
-                greater++;
-            }else{
-                less++;
-            }
-        }
-        int[] lArr=new int[less];
-        int[] gArr=new int[greater];
         int[] ans=new int[nums.length];
         int itr=0;
         for(int i : nums){
-            if(i>pivot){
-                gArr[itr++]=i;
-
-            }
-        }
-        itr=0;
-        for(int i : nums){
             if(i<pivot){
-                lArr[itr++]=i;
-
+                ans[itr++]=i;
             }
         }
-        itr=0;
-        for(int i=0;i<lArr.length;i++){
-            ans[itr++]=lArr[i];
+        for(int i : nums){
+            if(i==pivot){
+                ans[itr++]=i;
+            }
         }
-        for(int i=0;i<equal;i++){
-            ans[itr++]=pivot;
-        }
-        for(int i=0;i<gArr.length;i++){
-            ans[itr++]=gArr[i];
+        for(int i : nums){
+            if(i>pivot){
+                ans[itr++]=i;
+            }
         }
         return ans;
         
