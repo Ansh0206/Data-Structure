14197e5a6e4bbdef2d160fa9d78fec49edd49406
Ansh <148424504+Ansh0206@users.noreply.github.com>
2025-12-28 12:28:34 +0530

    Time: 0 ms (100%), Space: 42.8 MB (34.18%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int helper(int[] nums,int start,int end){
-        int last=0;
-        int secondLast=0;
-        int ans=-1;
-        for(int i=start;i<=end;i++){
-            ans=Math.max(secondLast,nums[i]+last);
-            last=secondLast;
-            secondLast=ans;
+    public int helper(int[] nums,int start,int end,Integer[] dp){
+        if(start>end){
+            return 0;
         }
-        return ans;
+        //rob current
+        if(dp[start]!=null){
+            return dp[start];
+        }
+        int rob=nums[start]+helper(nums,start+2,end,dp);
+        //not rob
+        int notRob=helper(nums,start+1,end,dp);
+        return dp[start]=Math.max(rob,notRob);
     }
+
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1){
             return nums[0];
         }
-        if(n==2){
-            return Math.max(nums[0],nums[1]);
-        }
-        int ans1=helper(nums,0,n-2);
-        int ans2=helper(nums,1,n-1);
+        Integer[] dp1=new Integer[n];
+        Integer[] dp2=new Integer[n];
+        int ans1=helper(nums,0,n-2,dp1);
+        int ans2=helper(nums,1,n-1,dp2);
         return Math.max(ans1,ans2);
         
     }
