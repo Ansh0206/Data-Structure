b1d5ff4c0fbbbb6b97f92c39f4feb0e54c23b105
Ansh <148424504+Ansh0206@users.noreply.github.com>
2025-12-30 10:14:06 +0530

    Time: 24 ms (79.59%), Space: 55 MB (5.61%) - LeetHub

--- 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.java
+++ 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.java
@@ -1,30 +1,11 @@
 class Solution {
     public int minOperations(String s) {
-        if(s.length()==1){
-            if(s.charAt(0)=='a'){
-                return 0;
-            }else{
-                return 26-(s.charAt(0)-'a');
-            }
+        int min=-1;
+        for(int i=0;i<s.length();i++){
+            int curr=(26-(s.charAt(i)-'a'))%26;
+            min=Math.max(min,curr);
         }
-        char[] arr=s.toCharArray();
-        Arrays.sort(arr);
-        int count=0;
-        for(int i=0;i<arr.length-1;i++){
-            char curr=arr[i];
-            if(curr=='a'){
-                continue;
-            }
-            while(curr!=arr[i+1]){
-                curr++;
-                count++;
-            }
-        }
-        if(count==0 && arr[arr.length-1]=='a'){
-            return 0;
-        }
-        count+=26-(arr[arr.length-1]-'a');
-        return count;
+        return min;
         
     }
 }
