6c9c2e20fc7e020eb137b3597295077280ab90fb
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-01-02 01:25:46 +0530

    Time: 1 ms (17.5%), Space: 46.5 MB (11.45%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,28 +1,23 @@
 class Solution {
-    public int helper(int[] prices,int i,int buy,Integer[][] dp){
-        if(i==prices.length){
-            return 0;
-        }
-        if(dp[i][buy]!=null){
-            return dp[i][buy];
-        }
-        int profit=0;
-        if(buy==1){
-            int yes=-prices[i]+helper(prices,i+1,0,dp);
-            int no=helper(prices,i+1,1,dp);
-            profit=Math.max(profit,Math.max(yes,no));
-        }else{
-            int yes=prices[i]+helper(prices,i+1,1,dp);
-            int no=helper(prices,i+1,0,dp);
-            profit=Math.max(profit,Math.max(yes,no));
-        }
-        return dp[i][buy]=profit;
-
-    }
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        Integer[][] dp=new Integer[n][2];
-        return helper(prices,0,1,dp);
+        int[][] dp=new int[n+1][2];
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(j==1){
+                    int yes=-prices[i]+dp[i+1][0];
+                    int no=dp[i+1][1];
+                    dp[i][j]=Math.max(dp[i][j],Math.max(yes,no));
+                }else{
+                    int yes=prices[i]+dp[i+1][1];
+                    int no=dp[i+1][0];
+                    dp[i][j]=Math.max(dp[i][j],Math.max(yes,no));
+                }
+            }
+        }
+        return dp[0][1];
+
 
         
     }
