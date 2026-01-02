1e61b7e4714afd9dec7f787d2ec6827b59f31c5a
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-01-02 17:50:53 +0530

    Time: 1 ms (17.5%), Space: 46.7 MB (9.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int[][] dp=new int[n+1][2];
-        dp[n][0]=dp[n][1]=0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                if(j==1){
-                    int yes=-prices[i]+dp[i+1][0];
-                    int no=dp[i+1][1];
-                    dp[i][j]=Math.max(dp[i][j],Math.max(yes,no));
-                }else{
-                    int yes=prices[i]+dp[i+1][1];
-                    int no=dp[i+1][0];
-                    dp[i][j]=Math.max(dp[i][j],Math.max(yes,no));
-                }
-            }
+        int[][] dp=new int[n][2];
+        //1--> means you can buy the stock(you are not holding any stock)
+        //0--> means you cant buy the stock (you are holding a stock) sell it first to buy another stock
+
+        dp[0][0]=0;
+        dp[0][1]=-prices[0];
+        for(int i=1;i<dp.length;i++){
+            //two choices
+            //buy
+            dp[i][1]=Math.max(-prices[i]+dp[i-1][0],dp[i-1][1]);
+            //sell
+            dp[i][0]=Math.max(prices[i]+dp[i-1][1],dp[i-1][0]);
         }
-        return dp[0][1];
-
-
-        
+        return dp[n-1][0];  //obviously at the end you nt need to hold any stoch
+                            //whats the point of holding if you cant sell it further
+                            
     }
 }
