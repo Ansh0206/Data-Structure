b1d8199d154988cc28b5e750481f81b44eca4871
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-01-02 17:53:20 +0530

    Time: 0 ms (100%), Space: 45.5 MB (43.28%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int[][] dp=new int[n][2];
+        int[] curr=new int[2];
+        int[] prev=new int[2];
         //1--> means you can buy the stock(you are not holding any stock)
         //0--> means you cant buy the stock (you are holding a stock) sell it first to buy another stock
 
-        dp[0][0]=0;
-        dp[0][1]=-prices[0];
-        for(int i=1;i<dp.length;i++){
+        prev[0]=0;
+        prev[1]=-prices[0];
+        for(int i=1;i<prices.length;i++){
             //two choices
             //buy
-            dp[i][1]=Math.max(-prices[i]+dp[i-1][0],dp[i-1][1]);
+            curr[1]=Math.max(-prices[i]+prev[0],prev[1]);
             //sell
-            dp[i][0]=Math.max(prices[i]+dp[i-1][1],dp[i-1][0]);
+            curr[0]=Math.max(prices[i]+prev[1],prev[0]);
+            prev=curr;
         }
-        return dp[n-1][0];  //obviously at the end you nt need to hold any stoch
+        return prev[0];  //obviously at the end you nt need to hold any stoch
                             //whats the point of holding if you cant sell it further
-                            
+
     }
 }
