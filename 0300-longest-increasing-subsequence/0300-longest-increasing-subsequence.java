d2a8130c62235781f695ead1402508d8a136e638
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-01-07 00:25:39 +0530

    Time: 40 ms (55.42%), Space: 46.3 MB (34.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,33 +1,17 @@
 class Solution {
-// public int helper(int[] nums,int curr,int prev,int[][] dp){
-//     if(curr==nums.length){
-//         return 0;
-//     }
-//     if(dp[curr][prev+1]!=-1){
-//         return dp[curr][prev+1];
-//     }
-//     int take=0;
-//     if(prev==-1 || nums[curr]>nums[prev]){
-//         take=1+helper(nums,curr+1,curr,dp);
-//     }
-//     int not=helper(nums,curr+1,prev,dp);
-//     return dp[curr][prev+1]=Math.max(take,not);
-// }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[] prevArr=new int[n+1];
-        int[] currArr=new int[n+1];
-        for(int curr=n-1;curr>=0;curr--){
-            for(int prev=curr-1;prev>=-1;prev--){
-                int take=0;
-                if(prev==-1 || nums[curr]>nums[prev]){
-                    take=1+prevArr[curr+1];
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        int ans=1;
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i],1+dp[j]);
                 }
-                int not=prevArr[prev+1];
-                currArr[prev+1]=Math.max(take,not);
             }
-            prevArr=currArr.clone();
+            ans=Math.max(ans,dp[i]);
         }
-        return prevArr[0];
+        return ans;
     }
 }
