45f90eff1d37b6f05b4508a9e6f6e89c0124b6f1
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-01-06 23:24:41 +0530

    Time: 367 ms (13.2%), Space: 237.4 MB (5.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,25 @@
 class Solution {
-    public int helper(ArrayList<Integer> list,int n){
-        int start=0;
-        int end=list.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(list.get(mid)==n){
-                return mid;
-            }
-            if(list.get(mid)>n){
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
+    public int helper(int[] nums,int curr,int prev,int[][] dp){
+        if(curr==nums.length){
+            return 0;
         }
-        return start;
+        if(dp[curr][prev+1]!=-1){
+            return dp[curr][prev+1];
+        }
+        int take=0;
+        if(prev==-1 || nums[curr]>nums[prev]){
+            take=1+helper(nums,curr+1,curr,dp);
+        }
+        int not=helper(nums,curr+1,prev,dp);
+        return dp[curr][prev+1]=Math.max(take,not);
     }
     public int lengthOfLIS(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(nums[0]);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>list.get(list.size()-1)){
-                list.add(nums[i]);
-            }else{
-                int index=helper(list,nums[i]);
-                list.set(index,nums[i]);
-            }
+        int n=nums.length;
+        int[][] dp=new int[n][n+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
         }
-        return list.size();
+        return helper(nums,0,-1,dp);
+        
     }
 }
