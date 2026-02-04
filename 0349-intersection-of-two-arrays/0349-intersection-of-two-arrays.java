2125e836216f703edd02f7eff39a948d5819d248
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-04 15:51:36 +0530

    Time: 2 ms (97.04%), Space: 45 MB (52.12%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,35 +1,22 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int[] arr=new int[1001];
-        boolean[] arr1=new boolean[1001];
-        boolean[] arr2=new boolean[1001];
-        for(int i=0;i<nums1.length;i++){
-            int curr=nums1[i];
-            if(!arr1[curr]){
-                arr[curr]++;
-                arr1[curr]=true;
+        Set<Integer> set=new HashSet<>();
+        Set<Integer> res=new HashSet<>();
+        for(int i : nums1){
+            set.add(i);
+        }
+        for(int i : nums2){
+            if(set.contains(i)){
+                res.add(i);
             }
         }
-        for(int i=0;i<nums2.length;i++){
-            int curr=nums2[i];
-            if(!arr2[curr]){
-                arr[curr]++;
-                arr2[curr]=true;
-            }
-        }
-        int len=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==2){
-                len++;
-            }
-        }
-        int[] res=new int[len];
+        int[] arr=new int[res.size()];
         int i=0;
-        for(int j=0;j<arr.length;j++){
-            if(arr[j]==2){
-                res[i++]=j;
-            }
+        for(Integer in : res){
+            arr[i++]=in;
         }
-        return res;
+        return arr;
+
+        
     }
 }
