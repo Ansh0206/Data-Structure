c6dc0af4dbef39237a65f9bb816591ac17f1d7ef
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-06 11:28:31 +0530

    Time: 6 ms (23.4%), Space: 51.4 MB (87.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,38 +3,53 @@ class Solution {
     static int colL;
     static int[] dr={-1,1,0,0};
     static int[] dc={0,0,-1,1};
-    public void dfs(int row,int col,boolean[][] vis,char[][] grid){
-        if(row<0 || row>=rowL || col<0 || col>=colL || grid[row][col]=='0'){
-            return;
+    static char[] directions={'U','D','L','R'};
+
+    static class Pair{
+        int row;
+        int col;
+        public Pair(int row,int col){
+            this.row=row;
+            this.col=col;
         }
-        if(vis[row][col]==true){
-            return;
-        }
-        vis[row][col]=true;
-        
-        for(int i=0;i<=3;i++){
-            dfs(row+dr[i],col+dc[i],vis,grid);
+    }
+    public void bfs(char[][] grid,boolean[][] vis,int i,int j){
+        Queue<Pair> q=new LinkedList<>();
+        vis[i][j]=true;
+        q.offer(new Pair(i,j));
+
+        while(!q.isEmpty()){
+            Pair curr=q.poll();
+            int row=curr.row;
+            int col=curr.col;
+
+            for(int index=0;index<4;index++){
+                int nRow=row+dr[index];
+                int nCol=col+dc[index];
+
+                if(nRow>=0 && nCol>=0 && nRow<rowL && nCol<colL  && !vis[nRow][nCol] && grid[nRow][nCol]=='1'){
+                    vis[nRow][nCol]=true;
+                    q.offer(new Pair(nRow,nCol));
+                }
+            }
         }
     }
     public int numIslands(char[][] grid) {
         rowL=grid.length;
         colL=grid[0].length;
         boolean[][] vis=new boolean[rowL][colL];
+        
         int components=0;
         for(int i=0;i<rowL;i++){
             for(int j=0;j<colL;j++){
-                //System.out.println("hi");
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    dfs(i,j,vis,grid);
-                   // System.out.println("hi");
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    bfs(grid,vis,i,j);
                     components++;
+                    
                 }
-                
-               
             }
         }
         return components;
-
         
     }
 }
