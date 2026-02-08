814dcf467b2c1e113b8b7194a5c514087ac36206
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-09 00:45:22 +0530

    Time: 7 ms (26.61%), Space: 47.6 MB (17.62%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,40 +1,33 @@
 class Solution {
     static int rowL;
     static int colL;
+
     static int[] dr={-1,1,0,0};
     static int[] dc={0,0,-1,1};
-
-    public int helper(int[][] grid,int i,int j,boolean[][] vis){
-        if(i<0 || j<0 || i>=rowL || j>=colL){
+    public int dfs(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i==rowL || j==colL || grid[i][j]==0){
             return 1;
         }
-        if(grid[i][j]==0){
-            return 1;
-        }
-        if(vis[i][j]){
+        if(grid[i][j]==2){
             return 0;
         }
-        vis[i][j]=true;
-        int ans=0;
-        for(int index=0;index<=3;index++){
-            ans+=helper(grid,i+dr[index],j+dc[index],vis); 
+        grid[i][j]=2;
+        int len=0;
+        for(int index=0;index<4;index++){
+            len+=dfs(grid,i+dr[index],j+dc[index]);
         }
-        return ans;
+        return len;
     }
     public int islandPerimeter(int[][] grid) {
         rowL=grid.length;
         colL=grid[0].length;
-        boolean[][] vis=new boolean[rowL][colL];
-        int i=0;
-        int j=0;
-        for(i=0;i<rowL;i++){
-            for(j=0;j<colL;j++){
+        for(int i=0;i<rowL;i++){
+            for(int j=0;j<colL;j++){
                 if(grid[i][j]==1){
-                    return helper(grid,i,j,vis);
+                    return dfs(grid,i,j);
                 }
             }
         }
         return -1;
-        
     }
 }
