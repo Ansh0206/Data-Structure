ba55271bc6c50407729102a7bf9b5f43e1015191
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-09 00:32:43 +0530

    Time: 2 ms (67.12%), Space: 46.1 MB (97.69%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -4,30 +4,29 @@ class Solution {
     static int[] dr={-1,1,0,0};
     static int[] dc={0,0,-1,1};
 
-    public int helper(int[][] grid,int row, int col,boolean[][] vis){
-        if(row<0 || row>=rowL || col<0 || col>=colL || grid[row][col]==0){
+    public int dfs(int[][] grid,boolean[][] vis,int i,int j){
+        if(i<0 || j<0 || i==rowL || j==colL || grid[i][j]==0){
             return 0;
         }
-        if(vis[row][col]){
+        if(vis[i][j]==true){
             return 0;
         }
-
-        vis[row][col]=true;
-        int ans=1;
-        for(int i=0;i<=3;i++){
-            ans+=helper(grid,row+dr[i],col+dc[i],vis);
+        vis[i][j]=true;
+        int len=1;
+        for(int index=0;index<4;index++){
+            len+=dfs(grid,vis,i+dr[index],j+dc[index]);
         }
-        return ans;
+        return len;
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int max=0;
         rowL=grid.length;
         colL=grid[0].length;
         boolean[][] vis=new boolean[rowL][colL];
+        int max=0;
         for(int i=0;i<rowL;i++){
             for(int j=0;j<colL;j++){
-                if(grid[i][j]!=0 && vis[i][j]==false){
-                    max=Math.max(max,helper(grid,i,j,vis));
+                if(grid[i][j]==1){
+                    max=Math.max(max,dfs(grid,vis,i,j));
 
                 }
             }
