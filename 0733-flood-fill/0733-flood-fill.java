edce8bfe6e39a5e93158041bb23e22aee9bdafc7
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-09 00:08:01 +0530

    Time: 1 ms (45.51%), Space: 46.8 MB (60.36%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,31 +3,27 @@ class Solution {
     static int colL;
     static int[] dr={-1,1,0,0};
     static int[] dc={0,0,-1,1};
-    public void helper(int[][] image,boolean[][] vis,int row,int col,int color,int preColor){
-        if(row<0 || row>=rowL || col<0 || col>=colL || image[row][col]!=preColor){
+    public void dfs(int[][] image,boolean[][] vis,int sr,int sc,int color,int prevColor){
+        if(sr<0 || sc<0 || sr==rowL || sc==colL || image[sr][sc]!=prevColor){
             return;
         }
-        if(vis[row][col]){
+        if(vis[sr][sc]){
             return;
         }
-        preColor=image[row][col];
-        vis[row][col]=true;
-        image[row][col]=color;
-        for(int i=0;i<=3;i++){
-            helper(image,vis,row+dr[i],col+dc[i],color,preColor);
+        vis[sr][sc]=true;
+        prevColor=image[sr][sc];
+        image[sr][sc]=color;
+        for(int i=0;i<4;i++){
+            dfs(image,vis,sr+dr[i],sc+dc[i],color,prevColor);
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         rowL=image.length;
         colL=image[0].length;
         boolean[][] vis=new boolean[rowL][colL];
-        int prev=image[sr][sc];
-        for(int i=sr;i<rowL;i++){
-            for(int j=sc;j<colL;j++){
-                helper(image,vis,i,j,color,image[sr][sc]);
-            }
-        }
+        dfs(image,vis,sr,sc,color,image[sr][sc]);
         return image;
+
         
     }
 }
