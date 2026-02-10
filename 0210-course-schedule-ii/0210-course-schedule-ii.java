c0b57ff19e912cc04571af8ecc685e0b2253e340
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-10 10:05:54 +0530

    Time: 5 ms (82.82%), Space: 46.9 MB (68.6%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        boolean[] vis=new boolean[numCourses];
+        for(int i=0;i<numCourses;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            list.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        int[] res=new int[numCourses];
+        int index=0;
+        int[] in=new int[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            in[prerequisites[i][0]]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<in.length;i++){
+            if(in[i]==0){
+                q.offer(i);
+                res[index]=i;
+                vis[index++]=true;
+            }
+        }
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            for(int i=0;i<list.get(curr).size();i++){
+                int el=list.get(curr).get(i);
+                in[el]--;
+                if(in[el]==0){
+                    q.add(el);
+                    res[index]=el;
+                    vis[index++]=true;
+                }  
+            }
+        }
+        for(int i=0;i<vis.length;i++){
+            if(vis[i]==false){
+                return new int[]{};
+            }
+        }
+        
+
+        return res;
+        
+    }
+}
