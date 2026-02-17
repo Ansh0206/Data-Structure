eddf08d1df98c32e6b51072b049e16e843e2e8f4
Ansh <148424504+Ansh0206@users.noreply.github.com>
2026-02-18 00:59:46 +0530

    Time: 0 ms (100%), Space: 44.8 MB (70.5%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,40 +1,45 @@
 class Solution {
-    int[] parent,rank;
+    static int[] parent;
+    static int[] rank;
+
+    public int find(int x){
+        if(parent[x]==x){
+            return  x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+    public boolean union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv){   //cycle exist
+           return false;
+        }
+        if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }else{
+            if(rank[pu]==rank[pv]){
+                rank[pv]++;
+            }
+            parent[pu]=pv;
+        }
+        return true;
+    }
     public int[] findRedundantConnection(int[][] edges) {
         int n=edges.length;
         parent=new int[n+1];
         rank=new int[n+1];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             parent[i]=i;
             rank[i]=0;
         }
         for(int[] e : edges){
             if(!union(e[0],e[1])){
-                return e;
+                return new int[]{e[0],e[1]};
             }
         }
         return new int[]{};
-    }
-    int find(int x){
-        if(parent[x]==x){
-            return x;
-        }
-        return parent[x]=find(parent[x]);
-    }
-    boolean union(int x,int y){
-        int px=find(x);
-        int py=find(y);
-        if(px==py){
-            return false;
-        }
-        if(rank[px]>rank[py]){
-            parent[py]=px;
-        }else{
-            if(parent[px]==parent[py]){
-                parent[px]++;
-            }
-            parent[px]=py;
-        }
-        return true;
+
+
+        
     }
 }
